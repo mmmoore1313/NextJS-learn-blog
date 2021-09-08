ca3284bbb3a3213a5c446cde230bee8d9fbb5335
Matt Moore <Matt.M.Moore13@outlook.com>
2021-09-08 17:06:54 -0400

    Redefined the return using Head; filled out some of the page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,20 @@ import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-
+    <Layout home>
+      <Head>
+        <title>
+          {siteTitle}
+        </title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi! I'm Matt- I'm a fullstack engineer
+        </p>
+        <p>
+          (This is a sample website - you'll be building a site like this on{' '}<a href='https://nextjs.org/learn'>our Next.js tutorial</a>)
+        </p>
+      </section>
+    </Layout>
   )
 }
