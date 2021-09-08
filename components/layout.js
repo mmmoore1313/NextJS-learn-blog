495a5500236414417e5e2a9f7700fcd31c904315
Matt Moore <Matt.M.Moore13@outlook.com>
2021-09-08 16:26:01 -0400

    Filled out the header with some JS logic

--- components/layout.js
+++ components/layout.js
@@ -14,16 +14,50 @@ export default function Layout({ children }) {
           content="Learn how to build a personal website using Next.js"
         />
         <meta
-          property='og:image'
+          property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name='og:title' content='siteTitle' />
+        <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <header className={styles.header}>
-        
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <a>
+                <Image
+                  priority
+                  src='/images/profile.jpg'
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href='/'>
+                <a className={utilStyles.colorInherit}>
+                  {name}
+                </a>
+              </Link>
+            </h2>
+          </>
+        )}
       </header>
       {children}
     </div>
